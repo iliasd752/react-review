f137694acd304460f8a90b5ec634217c796df227
Ilias <ilias.dinia@outlook.com>
2023-11-20 19:08:42 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+      <h1>My Blog</h1>
+      <article>
+        <h2>My first blog post</h2>
+        <p>This is the content of my post</p>
+      </article>
+        </div>
+    </div>
+  );
+}
+
+export default App;
